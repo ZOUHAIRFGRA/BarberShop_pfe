cf94afeebaf59bcaa5212547c84f7337b012e0f7
Zouhair Fouiguira <zouhairfgra@gmail.com>
2023-11-02 00:31:04 +0100

    Header Component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Footer from './components/Footer';
+import Header from './components/Header';
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl color-red font-bold underline">Hello world!</h1>
+      <Header/>
+      <div>
+        <h1 className='text-4xl'>Test</h1>
+      </div>
       <Footer />
     </>
   );
