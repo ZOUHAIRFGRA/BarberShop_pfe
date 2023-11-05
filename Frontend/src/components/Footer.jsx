5580dd09866dd62093d8d1084d5750900e798c03
Zouhair Fouiguira <zouhairfgra@gmail.com>
2023-11-05 19:56:49 +0100

    fixed footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-     {/*  <footer class=" bg-gray-900  bottom-0 sticky  ">
+      <footer class=" bg-gray-900  bottom-0 sticky  ">
         <div class="max-w-2xl mx-auto text-white py-10">
           <div class="text-center">
-            <h3 class="text-3xl mb-3"> Download our fitness app </h3>
-            <p> Stay fit. All day, every day. </p>
+            <h3 class="text-3xl mb-3">Install Our BarberApp </h3>
+            <p> lorem ipsum lorem lorem</p>
             <div class="flex justify-center my-10">
               <div class="flex items-center border w-auto rounded-lg px-4 py-2 mx-2">
                 <img
@@ -36,7 +36,7 @@ const Footer = () => {
           <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
             <p class="order-2 md:order-1 mt-8 md:mt-0">
               {" "}
-              &copy; Beautiful Footer, 2021.{" "}
+              &copy; lorem ipsum dolot lorem.{" "}
             </p>
             <div class="order-1 md:order-2">
               <span class="px-2">About us</span>
@@ -45,75 +45,8 @@ const Footer = () => {
             </div>
           </div>
         </div>
-      </footer> */}
-      <div className="untitled-page flex flex-col bg-[rgb(22,23,24)] relative min-h-[195px]">
-      <div className="flex items-center justify-center font-normal text-[11.0625px] leading-[1.44] font-Inter text-[rgb(104,104,104)] text-center tracking-[0.07px] w-[116px] absolute left-0 right-0 top-[162px] bottom-4 mx-auto">
-        Switch to mobile view
-      </div>
-      <div className="bg-[rgb(235,235,235)] border-solid border-[rgb(235,235,235)] border-t-[0px] border-r-[0px] border-b-[1px] border-l-[0px] w-[1820px] h-px opacity-[0.2] absolute left-[50px] top-0" />
-      <div className="flex flex-col bg-[rgb(194,194,194)] rounded-[20px] w-10 h-10 absolute left-[1830px] top-[104px]">
-        <img
-          className="w-[22px] h-[18px] object-cover my-[11px] mx-[9px]"
-          src={'/assets/e221d612743219da4f4fd4d50cdff99b.png'}
-          alt="alt text"
-        />
-      </div>
-      <div className="flex flex-col bg-[rgb(194,194,194)] rounded-[20px] w-10 h-10 absolute left-[1774px] top-[104px]">
-        <img
-          className="w-[11px] h-5 object-cover mt-2.5 mr-3.5 mb-2.5 ml-[15px]"
-          src={'/assets/b3bd5da60fc9cbcc110af8af79a31a1c.png'}
-          alt="alt text"
-        />
-      </div>
-      <div className="flex flex-col bg-[rgb(194,194,194)] rounded-[20px] w-10 h-10 absolute left-[1718px] top-[104px]">
-        <img
-          className="w-5 h-5 object-cover m-2.5"
-          src={'https://cdn-icons-png.flaticon.com/512/888/888857.png'}
-          alt="alt text"
-        />
-      </div>
-      <div className="flex items-center font-normal text-[9.21875px] leading-[1.51] font-Inter text-white tracking-[-0.05px] w-[171px] h-3.5 opacity-[0.5] absolute left-[161px] top-[117px]">
-        © 2023 BarberShop Inc. All rights reserved
-      </div>
-      <img
-        className="w-[141px] h-12 absolute left-[1729px] top-[22px]"
-        src={'https://cdn-icons-png.flaticon.com/512/888/888857.png'}
-        alt="alt text"
-      />
-      <img
-        className="w-[130px] h-[49px] absolute left-[1591px] top-[21px]"
-        src={'/assets/98746297125ba2fbd5e56490e69934dd.png'}
-        alt="alt text"
-      />
-      <div className="flex items-center justify-end font-normal text-[10.875px] leading-[1.65] font-Inter text-[rgb(219,219,219)] text-right tracking-[0.09px] w-[42px] h-[18px] absolute left-[516px] top-[38px]">
-        Contact
-      </div>
-      <div className="flex items-center justify-end font-normal text-[10.875px] leading-[1.65] font-Inter text-[rgb(219,219,219)] text-right tracking-[-0.01px] w-[88px] h-[18px] absolute left-[330px] top-[38px]">
-        Terms of Service
-      </div>
-      <div className="flex items-center font-normal text-[11.4375px] leading-[1.57] font-Inter text-[rgb(219,219,219)] tracking-[0.09px] w-[25px] h-[18px] absolute left-[50px] top-[38px]">
-        Blog
-      </div>
-      <div className="flex items-center font-normal text-[10.875px] leading-[1.65] font-Inter text-[rgb(219,219,219)] tracking-[0.09px] w-[73px] h-[18px] absolute left-[232px] top-[38px]">
-        Privacy Policy
-      </div>
-      <div className="flex items-center font-normal text-[12px] leading-normal font-Inter text-[rgb(219,219,219)] tracking-[0.09px] w-6 h-[18px] absolute left-[180px] top-[38px]">
-        FAQ
-      </div>
-      <div className="flex items-center justify-end font-normal text-[11.25px] leading-[1.6] font-Inter text-[rgb(219,219,219)] text-right tracking-[0.09px] w-[43px] h-[18px] absolute left-[445px] top-[38px]">
-        Careers
-      </div>
-      <img
-        className="w-[83px] h-[19px] absolute left-[50px] top-28"
-        src={'/assets/35dadd57cd34d5ff159dd1849019e3d9.png'}
-        alt="alt text"
-      />
-      <div className="bg-[rgb(235,235,235)] border-solid border-[rgb(235,235,235)] border-t-[0px] border-r-[0px] border-b-[1px] border-l-[0px] w-[1820px] h-px opacity-[0.2] absolute left-[50px] top-[89px]" />
-      <div className="flex items-center font-normal text-[11.0625px] leading-[1.62] font-Inter text-[rgb(219,219,219)] tracking-[-0.01px] w-[49px] h-[18px] absolute left-[103px] top-[38px]">
-        About Us
-      </div>
-    </div>
-
+      </footer> 
+      
     </>
   );
 
