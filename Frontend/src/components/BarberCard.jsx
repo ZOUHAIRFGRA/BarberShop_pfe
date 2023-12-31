d717c4184c3290398975360ddd34958ac26c4c44
Zouhair Fouiguira <zouhairfgra@gmail.com>
2023-12-31 18:47:25 +0100

    Add BarberCard component

---
+++ Frontend/src/components/BarberCard.jsx
@@ -0,0 +1,53 @@
+// src/components/BarberCard.js
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+const BarberCard = ({ barber }) => {
+  const { name, location, rating, reviewCount, image, services } = barber;
+
+  return (
+    <div className="card mb-3">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img src={image} alt={`${name} - ${location}`} className="img-fluid rounded-start" />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <div className="d-flex justify-content-between align-items-center mb-3">
+              <h5 className="card-title">{name}</h5>
+              <div className="text-end">
+                <div>
+                  {Array.from({ length: Math.floor(rating) }, (_, index) => (
+                    <FontAwesomeIcon key={index} icon={faStar} color="#ffc107" />
+                  ))}
+                </div>
+                <div>{reviewCount} reviews</div>
+              </div>
+            </div>
+            <p className="card-text">{location}</p>
+            <hr />
+            <ul className="list-unstyled">
+              {services.map((service, index) => (
+                <li className="mb-3" key={index}>
+                  <div className="d-flex justify-content-between">
+                    <div>
+                      <h6>{service.name}</h6>
+                      <p>{service.description}</p>
+                    </div>
+                    <div className="d-flex align-items-center">
+                      <span className="me-3">${service.price}</span>
+                      <button className="btn btn-primary">Book</button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BarberCard;
