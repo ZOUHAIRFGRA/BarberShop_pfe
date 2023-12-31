e7bbac1a7d7cc081043a247ed6a45d5b54f2ee9b
Zouhair Fouiguira <zouhairfgra@gmail.com>
2023-12-31 18:48:01 +0100

    Update App.js to use BarbersList instead of Register

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Register from './pages/Register';
+// import Register from './pages/Register';
+import BarbersList from './pages/BarbersList';
+
 export default function App() {
   return (
     <>
       <Header/>
-      <Register/>
+      <BarbersList/>
       <Footer />
     </>
   );
