8a5a8922b0237b0501c3a14c738d9af7abb02ffa
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-03 01:21:18 +0100

    Add navigation to barber details page in BarbersList.jsx

--- Frontend/src/pages/BarbersList.jsx
+++ Frontend/src/pages/BarbersList.jsx
@@ -1,14 +1,14 @@
 // src/pages/BarbersList.js
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import ReactPaginate from "react-paginate";
 import BarberCard from "../components/BarberCard";
-import barberData from '../data/barberData.json'
-
+import barberData from '../data/barberData.json';
 
 const BarbersList = () => {
-
+  const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useState(0);
-  const itemsPerPage = 5; 
+  const itemsPerPage = 5;
 
   const startIndex = currentPage * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -18,11 +18,22 @@ const BarbersList = () => {
     setCurrentPage(selected);
   };
 
+  const handleBarberCardClick = (barberId) => {
+    // Redirect to the barber details page
+    navigate.push(`/barber/${barberId}`);
+  };
+
   return (
     <div className="container mt-5">
       <div className="d-flex justify-content-center align-items-center flex-column mb-4">
         {displayedBarbers.map((barber, index) => (
-          <BarberCard key={index} barber={barber} />
+          <div
+            key={index}
+            onClick={() => handleBarberCardClick(barber.id)}
+            style={{ cursor: 'pointer' }}
+          >
+            <BarberCard barber={barber} />
+          </div>
         ))}
       </div>
 
