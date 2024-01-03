e3a9043d077c906ba588f4e89ac33496d11a09e4
zouhair fouiguira <zouhairfgra@gmail.com>
2024-01-03 17:06:57 +0100

    Adjust route for BarbersList and add NoPage component

--- Frontend/src/routes/Routes.js
+++ Frontend/src/routes/Routes.js
@@ -5,15 +5,16 @@ import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import NeighborhoodsPage from '../pages/NeighborhoodsPage';
 import BarbersList from '../pages/BarbersList';
-
+import NoPage from '../pages/NoPage';
 
 const Routes = () => {
   return (
     <RouterRoutes>
       <Route path="/" element={<HomePage />} />
       <Route path="/neighborhoods/:city" element={<NeighborhoodsPage />} />
-      {/* <Route path="/barber/:id" component={BarberDetails} /> */}
-      <Route path="/barbers" component={BarbersList} />
+      {/* Adjust the route for BarbersList */}
+      <Route path="/neighborhoods/:city/:neighborhood" element={<BarbersList />} />
+      <Route path="*" element={<NoPage />} />
     </RouterRoutes>
   );
 };
