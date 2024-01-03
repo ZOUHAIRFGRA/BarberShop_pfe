6b5ab4bada845954ea24ee0755d4ab8119033ae9
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-03 23:24:40 +0100

    Refactor Footer component and update styles

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,54 +1,83 @@
 // src/components/Footer.js
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-light py-4  w-100">
-    <div className="container">
-      <div className="row">
-          <div className="col-md-6">
-            <p className="mb-0">Your Company Name</p>
-          </div>
-          <div className="col-md-5 offset-md-1 mb-3">
-            <form>
-              <h5>Subscribe to our newsletter</h5>
-              <p>Monthly digest of what's new and exciting from us.</p>
-              <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                <label htmlFor="newsletter1" className="visually-hidden">
-                  Email address
-                </label>
-                <input
-                  id="newsletter1"
-                  type="text"
-                  className="form-control"
-                  placeholder="Email address"
-                />
-                <button className="btn btn-primary" type="button">
-                  Subscribe
-                </button>
-              </div>
-            </form>
+    <div className="footer-wrapper">
+      <footer className="bg-dark text-light py-3 w-100 flex-shrink-0">
+        <div className="container">
+          <div className="row gy-4 gx-5">
+            <div class="col-lg-4 col-md-6">
+              <h5 class="h1 text-white">BmB</h5>
+              <p class="small text-white">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt.
+              </p>
+              <p class="small text-white mb-0">
+                &copy; Copyrights. All rights reserved.{" "}
+                <a class="text-primary" href="_">
+                  BookMyBarber.com
+                </a>
+              </p>
+            </div>
+            <div class="col-lg-2 col-md-6">
+              <h5 class="text-white mb-3">Quick links</h5>
+              <ul class="list-unstyled text-muted">
+                <li>
+                  <a href="#_">Home</a>
+                </li>
+                <li>
+                  <a href="#_">About</a>
+                </li>
+                <li>
+                  <a href="#_">Get started</a>
+                </li>
+                <li>
+                  <a href="#_">FAQ</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-2 col-md-6">
+              <h5 class="text-white mb-3">Quick links</h5>
+              <ul class="list-unstyled text-muted">
+                <li>
+                  <a href="#_">Home</a>
+                </li>
+                <li>
+                  <a href="#_">About</a>
+                </li>
+                <li>
+                  <a href="#_">Get started</a>
+                </li>
+                <li>
+                  <a href="#_">FAQ</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+                  <label htmlFor="newsletter1" className="visually-hidden">
+                    Email address
+                  </label>
+                  <input
+                    id="newsletter1"
+                    type="text"
+                    className="form-control"
+                    placeholder="Email address"
+                  />
+                  <button className="btn btn-primary" type="button">
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-        <div className="row mt-3">
-          <div className="col-md-6">
-            {/* Links section */}
-            <ul className="list-unstyled d-flex">
-              <li className="me-3">
-                <a href="#_">Home</a>
-              </li>
-              <li className="me-3">
-                <a href="#_">About</a>
-              </li>
-              <li className="me-3">
-                <a href="#_">Blog</a>
-              </li>
-              {/* Add more links as needed */}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 };
 
