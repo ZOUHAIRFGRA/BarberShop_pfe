c42a76aa256b4f70bae5b62bb56c02ae9bc6759a
zouhair fouiguira <zouhairfgra@gmail.com>
2024-01-03 17:51:28 +0100

    Refactor header and register components

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,54 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      <div class="container-fluid">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-          <div class="col-md-3 mb-2 mb-md-0">
-            <a
-              href="/"
-              class="d-inline-flex link-body-emphasis text-decoration-none"
+      <div className="container-fluid">
+        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+          <div className="col-md-3 mb-2 mb-md-0">
+            <Link
+              to="/"
+              className="d-inline-flex link-body-emphasis text-decoration-none"
             >
               <h3>logo</h3>
-            </a>
+            </Link>
           </div>
 
-          <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
             <li>
-              <a href="/home" class="nav-link px-2 link-secondary">
+              <Link to="/home" className="nav-link px-2 link-secondary">
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="/features" class="nav-link px-2">
-                Features
-              </a>
+              <Link to="/home" className="nav-link px-2 link-secondary">
+                Home
+              </Link>
             </li>
             <li>
-              <a href="/err" class="nav-link px-2">
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a href="/err" class="nav-link px-2">
-                FAQs
-              </a>
-            </li>
-            <li>
-              <a href="#_" class="nav-link px-2">
-                About
-              </a>
+              <Link to="/home" className="nav-link px-2 link-secondary">
+                Home
+              </Link>
             </li>
           </ul>
 
-          <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2">
+          <div className="col-md-3 text-end">
+            <Link to="/login" className="btn btn-outline-primary me-2">
               Login
-            </button>
-            <button type="button" class="btn btn-primary">
+            </Link>
+            <Link to="/register" className="btn btn-primary">
               Sign-up
-            </button>
+            </Link>
           </div>
         </header>
       </div>
