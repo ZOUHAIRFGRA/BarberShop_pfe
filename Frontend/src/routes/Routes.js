90084f895b6b6e3b08649093fa6d3d2208431233
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-03 21:46:39 +0100

    Add BarberDetails page and update routes

--- Frontend/src/routes/Routes.js
+++ Frontend/src/routes/Routes.js
@@ -5,6 +5,7 @@ import { Route, Routes as RouterRoutes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import NeighborhoodsPage from '../pages/NeighborhoodsPage';
 import BarbersList from '../pages/BarbersList';
+import BarberDetails from '../pages/BarberDetails'; // Import the new page
 import NoPage from '../pages/NoPage';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
@@ -16,6 +17,8 @@ const Routes = () => {
       <Route path="/neighborhoods/:city" element={<NeighborhoodsPage />} />
       {/* Adjust the route for BarbersList */}
       <Route path="/neighborhoods/:city/:neighborhood" element={<BarbersList />} />
+      {/* New route for BarberDetails */}
+      <Route path="/barber/:city/:neighborhood/:id" element={<BarberDetails />} />
       <Route path="*" element={<NoPage />} />
 
       <Route path='/login' element={<Login/>} />
