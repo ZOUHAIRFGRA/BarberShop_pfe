2903827aa730960f0da43ea950a11f0f78c0582d
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-03 23:24:59 +0100

    Update Login and Register forms

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,12 +1,53 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    console.log('Login clicked');
+  };
+
   return (
-    
-    <h1>Login Form</h1>
+    <div className="container mt-5 w-50 border border-1">
+      <h2 className='text-center mt-4'>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className='text-center mb-4'>
+        <button type="submit" className="btn btn-primary ">
+          Login
+        </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
