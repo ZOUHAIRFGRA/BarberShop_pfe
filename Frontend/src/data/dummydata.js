7a6b4fb866ad1b458dd39e6f1fe17d4499e58ea3
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-03 22:34:26 +0100

    Fix bug in login functionality

--- Frontend/src/data/dummydata.js
+++ Frontend/src/data/dummydata.js
@@ -8,6 +8,7 @@ export const cities = [
         name: "Anfa",
         barbers: [
           {
+            id:1,
             name: "Ahmed's Barbershop Anfa Casa",
             rating: 4.5,
             reviewCount: 30,
@@ -26,10 +27,11 @@ export const cities = [
             ],
           },
           {
+            id:2,
             name: "Ahmed's Barbershop Anfa Casa",
             rating: 4.5,
             reviewCount: 30,
-            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?w=740&t=st=1704041446~exp=1704042046~hmac=02ab25eb70931cd5dfca374f9149171722abe4817680ac0b315378f5845ea5bc",
+            image: "https://img.freepik.com/free-photo/man-wearing-waistcoat_1368-2886.jpg?w=740&t=st=1704315415~exp=1704316015~hmac=5d0968ea3ed23d898fd60bd1918a05567773590df12651d8d1e9aadbb4cd0d91",
             services: [
               {
                 name: "Haircut",
@@ -44,6 +46,7 @@ export const cities = [
             ],
           },
           {
+            id:3,
             name: "Ahmed's Barbershop Anfa Casa",
             rating: 4.5,
             reviewCount: 30,
@@ -68,10 +71,11 @@ export const cities = [
         name: "El Oulfa",
         barbers: [
           {
+            id:4,
             name: "Ali's Barbershop El Oulfa casa",
             rating: 4.2,
             reviewCount: 25,
-            image: "https://example.com/ali.jpg",
+            image: "https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=740&t=st=1704315301~exp=1704315901~hmac=01733a25c2cb1b6925882d47572006ec6677e1026ba3bc0729663d13e7714aa6",
             services: [
               {
                 name: "Haircut",
@@ -92,10 +96,11 @@ export const cities = [
         name: "Hay Hassani",
         barbers: [
           {
+            id:5,
             name: "Ali's Barbershop Hay H casa",
             rating: 4.2,
             reviewCount: 25,
-            image: "https://example.com/ali.jpg",
+            image: "https://img.freepik.com/free-photo/handsome-bearded-guy-posing-against-white-wall_273609-20597.jpg?w=740&t=st=1704315340~exp=1704315940~hmac=e02ebb812a459a098d152a661ce56d52713af40d45c854db4cd32547bc81932e",
             services: [
               {
                 name: "Haircut",
@@ -116,10 +121,11 @@ export const cities = [
         name: "Hay Mohammadi",
         barbers: [
           {
+            id:6,
             name: "Ali's Barbershop HM casa",
             rating: 4.2,
             reviewCount: 25,
-            image: "https://example.com/ali.jpg",
+            image: "https://img.freepik.com/free-photo/portrait-successful-man-having-stubble-posing-with-broad-smile-keeping-arms-folded_171337-1267.jpg?w=740&t=st=1704315362~exp=1704315962~hmac=9cf248927328dd6c25d0d50ee3b4326aac6fe5ff9d95984f1de8cc63bce62578",
             services: [
               {
                 name: "Haircut",
@@ -146,10 +152,11 @@ export const cities = [
         name: "Agdal",
         barbers: [
           {
+            id:7,
             name: "Mounir's Styling Agdal",
             rating: 4.8,
             reviewCount: 45,
-            image: "https://example.com/mounir.jpg",
+            image: "https://img.freepik.com/free-photo/full-length-portrait-cheerful-confident-man_171337-4239.jpg?w=360&t=st=1704315381~exp=1704315981~hmac=fddcfa0fe83f6ddc3a8ec57c56f0119c03418fdfd66d3a0a985ea7739ff81f58",
             services: [
               {
                 name: "Haircut",
@@ -170,10 +177,11 @@ export const cities = [
         name: "Centre Ville",
         barbers: [
           {
+            id:8,
             name: "Khalid's Barbershop Cv",
             rating: 4.6,
             reviewCount: 40,
-            image: "https://example.com/khalid.jpg",
+            image: "https://img.freepik.com/premium-psd/man-giving-thumbs-up-sign-with-picture-him_176841-36311.jpg?w=740",
             services: [
               {
                 name: "Haircut",
@@ -194,10 +202,11 @@ export const cities = [
         name: "AL wifaq",
         barbers: [
           {
+            id:9,
             name: "Khalid's Barbershop aW",
             rating: 4.6,
             reviewCount: 40,
-            image: "https://example.com/khalid.jpg",
+            image: "https://img.freepik.com/free-photo/man-wearing-waistcoat_1368-2886.jpg?w=740&t=st=1704315415~exp=1704316015~hmac=5d0968ea3ed23d898fd60bd1918a05567773590df12651d8d1e9aadbb4cd0d91",
             services: [
               {
                 name: "Haircut",
@@ -218,10 +227,11 @@ export const cities = [
         name: "Harhoura",
         barbers: [
           {
+            id:10,
             name: "Khalid's Barbershop H",
             rating: 4.6,
             reviewCount: 40,
-            image: "https://example.com/khalid.jpg",
+            image: "https://img.freepik.com/free-photo/handsome-man-presenting-something_1368-87.jpg?w=360&t=st=1704315434~exp=1704316034~hmac=7020ece91a652c37feee86a3f460d0455e17ebc2b231659d72890ac40a822ef3",
             services: [
               {
                 name: "Haircut",
@@ -248,10 +258,11 @@ export const cities = [
         name: "Hay mohamadi",
         barbers: [
           {
+            id:11,
             name: "Youssef's Salon Hm Ag",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?size=626&ext=jpg&ga=GA1.1.1882323155.1704038130&semt=sph",
             services: [
               {
                 name: "Haircut",
@@ -272,10 +283,11 @@ export const cities = [
         name: "Essalam",
         barbers: [
           {
+            id:12,
             name: "Youssef's Salon Essalam",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/man-wearing-waistcoat_1368-2886.jpg?w=740&t=st=1704315415~exp=1704316015~hmac=5d0968ea3ed23d898fd60bd1918a05567773590df12651d8d1e9aadbb4cd0d91",
             services: [
               {
                 name: "Haircut",
@@ -296,10 +308,11 @@ export const cities = [
         name: "EL houda",
         barbers: [
           {
+            id:13,
             name: "Youssef's Salon el houda",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?size=626&ext=jpg&ga=GA1.1.1882323155.1704038130&semt=sph",
             services: [
               {
                 name: "Haircut",
@@ -320,10 +333,11 @@ export const cities = [
         name: "tabrjt",
         barbers: [
           {
+            id:14,
             name: "Youssef's Salon tabrjt",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?size=626&ext=jpg&ga=GA1.1.1882323155.1704038130&semt=sph",
             services: [
               {
                 name: "Haircut",
@@ -344,10 +358,11 @@ export const cities = [
         name: "inzegane",
         barbers: [
           {
+            id:15,
             name: "Youssef's Salon inze",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?size=626&ext=jpg&ga=GA1.1.1882323155.1704038130&semt=sph",
             services: [
               {
                 name: "Haircut",
@@ -368,10 +383,11 @@ export const cities = [
         name: "Edcheira",
         barbers: [
           {
+            id:16,
             name: "Youssef's Salon dch",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?size=626&ext=jpg&ga=GA1.1.1882323155.1704038130&semt=sph",
             services: [
               {
                 name: "Haircut",
@@ -392,10 +408,11 @@ export const cities = [
         name: "Drarga",
         barbers: [
           {
+            id:17,
             name: "Youssef's Salon drar",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?size=626&ext=jpg&ga=GA1.1.1882323155.1704038130&semt=sph",
             services: [
               {
                 name: "Haircut",
@@ -416,10 +433,11 @@ export const cities = [
         name: "Tagadirt",
         barbers: [
           {
+            id:18,
             name: "Youssef's Salon tag",
             rating: 4.3,
             reviewCount: 35,
-            image: "https://example.com/youssef.jpg",
+            image: "https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?size=626&ext=jpg&ga=GA1.1.1882323155.1704038130&semt=sph",
             services: [
               {
                 name: "Haircut",
@@ -446,6 +464,7 @@ export const cities = [
         name: "Medina",
         barbers: [
           {
+            id:19,
             name: "Karim's Barbershop kech medin",
             rating: 4.7,
             reviewCount: 38,
