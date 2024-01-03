26e42762b039521b2018c3fe32e3815cbf3f1362
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-03 01:20:29 +0100

    Update user authentication logic

---
+++ Frontend/src/data/dummydata.js
@@ -0,0 +1,437 @@
+// data.js
+
+export const cities = [
+  {
+    name: "Casablanca",
+    neighborhoods: [
+      {
+        name: "Anfa",
+        barbers: [
+          {
+            name: "Ahmed's Barbershop",
+            rating: 4.5,
+            reviewCount: 30,
+            image: "https://example.com/ahmed.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "50.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "30.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Anfa as needed
+        ],
+      },
+      {
+        name: "El Oulfa",
+        barbers: [
+          {
+            name: "Ali's Barbershop",
+            rating: 4.2,
+            reviewCount: 25,
+            image: "https://example.com/ali.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "45.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "25.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in El Oulfa as needed
+        ],
+      },
+      {
+        name: "Hay Hassani",
+        barbers: [
+          {
+            name: "Ali's Barbershop",
+            rating: 4.2,
+            reviewCount: 25,
+            image: "https://example.com/ali.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "45.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "25.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in El Oulfa as needed
+        ],
+      },
+      {
+        name: "Hay Mohammadi",
+        barbers: [
+          {
+            name: "Ali's Barbershop",
+            rating: 4.2,
+            reviewCount: 25,
+            image: "https://example.com/ali.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "45.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "25.00",
+                duration: "30m",
+              },
+            ],
+          },
+          
+        ],
+      },
+      // Add more neighborhoods in Casablanca as needed
+    ],
+  },
+  {
+    name: "Rabat",
+    neighborhoods: [
+      {
+        name: "Agdal",
+        barbers: [
+          {
+            name: "Mounir's Styling",
+            rating: 4.8,
+            reviewCount: 45,
+            image: "https://example.com/mounir.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "40.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Grooming",
+                price: "35.00",
+                duration: "45m",
+              },
+            ],
+          },
+          // Add more barbers in Agdal as needed
+        ],
+      },
+      {
+        name: "Centre Ville",
+        barbers: [
+          {
+            name: "Khalid's Barbershop",
+            rating: 4.6,
+            reviewCount: 40,
+            image: "https://example.com/khalid.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "42.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "28.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Hassan as needed
+        ],
+      },
+      {
+        name: "AL wifaq",
+        barbers: [
+          {
+            name: "Khalid's Barbershop",
+            rating: 4.6,
+            reviewCount: 40,
+            image: "https://example.com/khalid.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "42.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "28.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Hassan as needed
+        ],
+      },
+      {
+        name: "Harhoura",
+        barbers: [
+          {
+            name: "Khalid's Barbershop",
+            rating: 4.6,
+            reviewCount: 40,
+            image: "https://example.com/khalid.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "42.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "28.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Hassan as needed
+        ],
+      },
+      // Add more neighborhoods in Rabat as needed
+    ],
+  },
+  {
+    name: "Agadir",
+    neighborhoods: [
+      {
+        name: "Hay mohamadi",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      {
+        name: "Essalam",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      {
+        name: "EL houda",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      {
+        name: "tabrjt",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      {
+        name: "inzegane",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      {
+        name: "Edcheira",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      {
+        name: "Drarga",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      {
+        name: "Tagadirt",
+        barbers: [
+          {
+            name: "Youssef's Salon",
+            rating: 4.3,
+            reviewCount: 35,
+            image: "https://example.com/youssef.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "48.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "32.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Downtown Agadir as needed
+        ],
+      },
+      // Add more neighborhoods in Agadir as needed
+    ],
+  },
+  {
+    name: "Marrakech",
+    neighborhoods: [
+      {
+        name: "Medina",
+        barbers: [
+          {
+            name: "Karim's Barbershop",
+            rating: 4.7,
+            reviewCount: 38,
+            image: "https://example.com/karim.jpg",
+            services: [
+              {
+                name: "Haircut",
+                price: "46.00",
+                duration: "1h",
+              },
+              {
+                name: "Beard Trim",
+                price: "30.00",
+                duration: "30m",
+              },
+            ],
+          },
+          // Add more barbers in Medina Marrakech as needed
+        ],
+      },
+      // Add more neighborhoods in Marrakech as needed
+    ],
+  },
+  // Add more cities as needed
+];
