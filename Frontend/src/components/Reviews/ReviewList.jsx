a4c6b40666fb4dd4fc1410cec704f1c183a275c6
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-15 20:09:36 +0100

    Refactor ReviewList component and add carousel for displaying customer reviews

--- Frontend/src/components/Reviews/ReviewList.jsx
+++ Frontend/src/components/Reviews/ReviewList.jsx
@@ -1,8 +1,8 @@
-// ReviewList.js
 import React, { useState, useEffect } from 'react';
 import ReviewCard from './ReviewCard';
+import './ReviewList.css'
 
-const ReviewList = ({ barberId }) => {
+const ReviewList = () => {
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
@@ -11,39 +11,66 @@ const ReviewList = ({ barberId }) => {
         const response = await fetch('/dummydata.json'); // Update the path accordingly
         const data = await response.json();
 
-        // Find the barber by ID
-        const barber = data.flatMap((city) =>
-          city.neighborhoods.flatMap((neighborhood) =>
-            neighborhood.barbers.find((b) => b.id === barberId)
+        // Assuming reviews are directly in the data (modify this based on your data structure)
+        const allReviews = data.flatMap(city =>
+          city.neighborhoods.flatMap(neighborhood =>
+            neighborhood.barbers.flatMap(barber =>
+              barber.reviews ? barber.reviews.map(review => ({ ...review, barberName: barber.name })) : []
+            )
           )
         );
 
-        // Check if the barber has reviews
-        const barberReviews = barber.reviews || [];
-
-        setReviews(barberReviews);
+        setReviews(allReviews);
       } catch (error) {
         console.error('Error fetching reviews:', error);
       }
     };
 
     fetchReviews();
-  }, [barberId]);
+  }, []);
 
   // Function to get 3 random reviews
   const getRandomReviews = () => {
     const shuffledReviews = reviews.sort(() => 0.5 - Math.random());
-    return shuffledReviews.slice(0, 3);
+    return shuffledReviews.slice(0, 10);
   };
 
   const randomReviews = getRandomReviews();
 
   return (
-    <div>
-      <h3>Reviews</h3>
-      {randomReviews.map((review) => (
-        <ReviewCard key={review.user_id} review={review} />
-      ))}
+    <div className='container pt-5'>
+      <h1 className='text-center pb-4'>Barbershops - customer reviews</h1>
+       <div id="reviewCarousel" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        {randomReviews.reduce((chunks, item, index) => {
+          const chunkIndex = Math.floor(index / 3);
+
+          if (!chunks[chunkIndex]) {
+            chunks[chunkIndex] = [];
+          }
+
+          chunks[chunkIndex].push(item);
+
+          return chunks;
+        }, []).map((chunk, index) => (
+          <div key={index} className={`carousel-item${index === 0 ? ' active' : ''}`}>
+            <div className="d-flex justify-content-around">
+              {chunk.map((review, idx) => (
+                <ReviewCard key={idx} review={review} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#reviewCarousel" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon bg-black" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#reviewCarousel" data-bs-slide="next">
+        <span className="carousel-control-next-icon bg-black" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
     </div>
   );
 };
