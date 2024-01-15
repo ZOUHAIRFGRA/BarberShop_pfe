038a88ae86da88415d8c908fe487b6f34d62a0e1
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-15 20:07:26 +0100

    Add Bootstrap dependencies

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle'; 
+import 'bootstrap-icons/font/bootstrap-icons.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap-select/dist/css/bootstrap-select.min.css'; 
+import 'bootstrap-select/dist/js/bootstrap-select.min'; 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
