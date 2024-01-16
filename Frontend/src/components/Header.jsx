785f2177839d09eca508c936b3f97a6ea67b9991
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-16 13:07:32 +0100

    Add React Bootstrap and FontAwesome icons

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,48 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Navbar,Container,Nav} from 'react-bootstrap'
 
 const Header = () => {
   return (
     <>
-      <div className="container-fluid">
-        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-          <div className="col-md-3 mb-2 mb-md-0">
-            <Link
-              to="/"
-              className="d-inline-flex link-body-emphasis text-decoration-none"
-            >
-              <h3>logo</h3>
-            </Link>
-          </div>
-
-          <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-            <li>
-              <Link to="/" className="nav-link px-2 link-secondary">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="nav-link px-2 link-secondary">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="nav-link px-2 link-secondary">
-                Home
-              </Link>
-            </li>
-          </ul>
-
-          <div className="col-md-3 text-end">
-            <Link to="/login" className="btn btn-outline-primary me-2">
-              Login
-            </Link>
-            <Link to="/register" className="btn btn-primary">
-              Sign-up
-            </Link>
-          </div>
-        </header>
-      </div>
+      <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">Link</Nav.Link>
+            <Nav.Link disabled>Disabled</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   );
 };
--- Frontend/src/pages/BarbersList.jsx
+++ Frontend/src/pages/BarbersList.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import BarberCard from "../components/BarberCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo, faHome } from "@fortawesome/free-solid-svg-icons";
 
 const BarbersList = () => {
   const { city, neighborhood } = useParams();
@@ -13,13 +15,15 @@ const BarbersList = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('/dummydata.json'); 
+        const response = await fetch("/dummydata.json");
         const data = await response.json();
 
         const cityData = data.find((c) => c.name === city);
 
         if (cityData) {
-          const neighborhoodData = cityData.neighborhoods.find((n) => n.name === neighborhood);
+          const neighborhoodData = cityData.neighborhoods.find(
+            (n) => n.name === neighborhood
+          );
 
           if (neighborhoodData) {
             setBarberData(neighborhoodData.barbers);
@@ -46,34 +50,62 @@ const BarbersList = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <div className="d-flex justify-content-center align-items-center flex-column mb-4">
-        {displayedBarbers.map((barber, index) => (
-          <div
-            key={index}
-            onClick={() => handleBarberCardClick(barber.id)}
-            style={{ cursor: 'pointer' }}
-          >
-            <BarberCard barber={barber} />
-          </div>
-        ))}
-      </div>
+    <>
+      <div className="container mt-5">
+        <div className="pb-5">
+          <h1 className="header ">
+            Barbershops & Barbers Near Me in {neighborhood}
+          </h1>
+          <p className="small">
+            What affects the search results?{" "}
+            <FontAwesomeIcon icon={faCircleInfo} size="sm" />{" "}
+          </p>
+          <hr />
+        </div>
 
-      <ReactPaginate
-        pageCount={Math.ceil(barberData.length / itemsPerPage)}
-        pageRangeDisplayed={10}
-        marginPagesDisplayed={2}
-        onPageChange={handlePageChange}
-        containerClassName={'pagination justify-content-center'}
-        activeClassName={'active'}
-        pageClassName={'page-item'}
-        pageLinkClassName={'page-link'}
-        previousClassName={'page-item'}
-        previousLinkClassName={'page-link'}
-        nextClassName={'page-item'}
-        nextLinkClassName={'page-link'}
-      />
-    </div>
+        <div className="d-flex justify-content-center align-items-center flex-column mb-4">
+          {displayedBarbers.map((barber, index) => (
+            <div
+              key={index}
+              onClick={() => handleBarberCardClick(barber.id)}
+              style={{ cursor: "pointer" }}
+            >
+              <BarberCard barber={barber} />
+            </div>
+          ))}
+        </div>
+
+        <ReactPaginate
+          pageCount={Math.ceil(barberData.length / itemsPerPage)}
+          pageRangeDisplayed={10}
+          marginPagesDisplayed={2}
+          onPageChange={handlePageChange}
+          containerClassName={"pagination justify-content-center"}
+          activeClassName={"active"}
+          pageClassName={"page-item"}
+          pageLinkClassName={"page-link"}
+          previousClassName={"page-item"}
+          previousLinkClassName={"page-link"}
+          nextClassName={"page-item"}
+          nextLinkClassName={"page-link"}
+        />
+        <div>
+          <p>
+            <Link to={"/"}>
+              <FontAwesomeIcon icon={faHome} />
+            </Link>{" "}
+            /{" "}
+            <Link
+              className="link-primary link-underline-opacity-0"
+              to={`/neighborhoods/${city}`}
+            >
+              Barbershop
+            </Link>{" "}
+            / Barbershops in {neighborhood}
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
