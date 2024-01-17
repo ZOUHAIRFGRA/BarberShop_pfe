a4921d2c03c45db9be3408c9a74bb5ef764d9093
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-17 23:14:37 +0100

    Update localisation component iframe attributes

--- Frontend/src/components/localisation.jsx
+++ Frontend/src/components/localisation.jsx
@@ -6,12 +6,13 @@ function App() {
       <div className="App">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53183.14994985655!2d-7.63970056367578!3d33.58072865725071!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda7d3a9223b51f5%3A0x55d379e0d8106412!2sThe%20BARBER%20SHOP%20-%20CASABLANCA%20(%20STAR%20)!5e0!3m2!1sen!2sus!4v1705086115581!5m2!1sen!2sus"
+          title="Barber Shop Location"
           width="100%"
           height="300"
           style={{ borderRadius: 5 }}
-          allowfullscreen=""
+          allowFullScreen=""
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
     </div>
