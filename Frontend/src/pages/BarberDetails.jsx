65d31fcbc39e0033721c7e7081b3c223d97809b7
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-21 00:54:08 +0100

    Rating part done

--- Frontend/src/pages/BarberDetails.jsx
+++ Frontend/src/pages/BarberDetails.jsx
@@ -236,6 +236,83 @@ const BarberDetails = () => {
               </div>
 
               {/* Rating  */}
+              <div data-aos="fade-up-right" className="col-12 col-md-6">
+                <div className="row ">
+                  <div className="content text-center mt-5  ">
+                    <div className="row border rounded shadow-sm m-lg-0 m-2">
+                      {/* Rating Section */}
+                      <div className="col-md-6">
+                        <div className="ratings bg-white py-lg-5 py-4 ">
+                          <span className="product-rating display-5">
+                            {rating}
+                          </span>
+                          <span className="h4">/5</span>
+                          <div className="stars">
+                            {Array.from(
+                              { length: Math.floor(rating) },
+                              (_, index) => (
+                                <FontAwesomeIcon
+                                  key={index}
+                                  icon={faStar}
+                                  color="#ffc107"
+                                />
+                              )
+                            )}
+                          </div>
+                          <div className="rating-text mt-3">
+                            <span className="h6">
+                              Based on {reviewCount} reviews
+                            </span>
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* Star Count Histogram Section */}
+                      <div className="col-md-6">
+                        <div className="star-counts  mt-lg-4 py-lg-4">
+                          {[5, 4, 3, 2, 1].map((star, index) => (
+                            <div
+                              key={index}
+                              className="star-count-item d-flex align-items-center"
+                            >
+                              <span className="star ">
+                                {star}{" "}
+                                <FontAwesomeIcon
+                                  key={index}
+                                  icon={faStar}
+                                  color="#ffc107"
+                                />
+                              </span>
+                              <div
+                                className="progress ml-2"
+                                style={{ flex: 1, height: "8px" }}
+                              >
+                                <div
+                                  className="progress-bar bg-warning"
+                                  role="progressbar"
+                                  style={{
+                                    width: `${calculateStarPercentage(
+                                      star,
+                                      reviews
+                                    )}%`,
+                                  }}
+                                  aria-valuenow={calculateStarPercentage(
+                                    star,
+                                    reviews
+                                  )}
+                                  aria-valuemin="0"
+                                  aria-valuemax="100"
+                                ></div>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      {/* End Star Count Histogram Section */}
+                    </div>
+                  </div>
+                </div>
+              </div>
              
             </div>
 
