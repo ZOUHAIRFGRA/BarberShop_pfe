ef3062e81fc15e070c659c695c5fdf867e166f69
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-21 17:21:30 +0100

    Add pagination to NeighborhoodsPage

--- Frontend/src/pages/NeighborhoodsPage.jsx
+++ Frontend/src/pages/NeighborhoodsPage.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import DynamicLinks from "../components/DynamicLinks/DynamicLinks";
 import ReviewList from "../components/Reviews/ReviewList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const NeighborhoodsPage = () => {
   const { city, neighborhood } = useParams();
@@ -45,16 +47,14 @@ const NeighborhoodsPage = () => {
           </p>
         </div>
         <div className="container text-center w-100 py-5">
-  <img
-    className="img-fluid"
-    src="/assets/img1.png"
-    alt="Barbershops near you"
-    title="Barbershops near you"
-  />
-</div>
-
+          <img
+            className="img-fluid"
+            src="/assets/img1.png"
+            alt="Barbershops near you"
+            title="Barbershops near you"
+          />
+        </div>
       </section>
-     
 
       {/* Pass selectedCity and neighborhoods data to DynamicLinks */}
       <DynamicLinks
@@ -64,41 +64,51 @@ const NeighborhoodsPage = () => {
       />
       <ReviewList />
       <section className="about-2 pt-5  ">
-        
-          <div className="container text-center w-100 py-5 ">
-            <img
+        <div className="container text-center w-100 py-5 ">
+          <img
             className="img-fluid"
-              src="/assets/img2.jpg"
-              alt="Barbershops near you"
-              title="Barbershops near you"
-            />
-          </div>
-          <div className="container py-5 w-75">
-            <h1 className="text-center pb-5 ">
-              What Services Do Barbers Provide?
-            </h1>
-            <p>
-              A barber is a professional who cuts or styles hair and grooms
-              beards, mustaches, and goatees.
-              <strong>Barbershops are an old American tradition.</strong> As a
-              result, many barbers offer classic and retro-style haircuts.
-              Barbers can also shave, shampoo, and color their client's hair and
-              beards. Barbers are also known for their customer service skills
-              and personability. If your beard is getting out of control (or you
-              want a professional to shave the hair off the back of your neck,
-              <strong> a visit to a barber is perfect!</strong>
-            </p>
-          </div>
-          <div className="container py-3 w-75">
-            <h1 className="text-center pb-5 ">
-            How to Find the Best Barber?
-            </h1>
-            <p>
-            At GoMyBarber, we make finding the perfect barbershop a total breeze. You can instantly view all the barbershops that are close to your home. You can check out their prices, services offered, and more, When looking for a barber, you should consider a few things. Take into consideration your price range, the location, reviews, and the services offered.
-            </p>
-          </div>
-        
+            src="/assets/img2.jpg"
+            alt="Barbershops near you"
+            title="Barbershops near you"
+          />
+        </div>
+        <div className="container py-5 w-75">
+          <h1 className="text-center pb-5 ">
+            What Services Do Barbers Provide?
+          </h1>
+          <p>
+            A barber is a professional who cuts or styles hair and grooms
+            beards, mustaches, and goatees.
+            <strong>Barbershops are an old American tradition.</strong> As a
+            result, many barbers offer classic and retro-style haircuts. Barbers
+            can also shave, shampoo, and color their client's hair and beards.
+            Barbers are also known for their customer service skills and
+            personability. If your beard is getting out of control (or you want
+            a professional to shave the hair off the back of your neck,
+            <strong> a visit to a barber is perfect!</strong>
+          </p>
+        </div>
+        <div className="container py-3 w-75">
+          <h1 className="text-center pb-5 ">How to Find the Best Barber?</h1>
+          <p>
+            At GoMyBarber, we make finding the perfect barbershop a total
+            breeze. You can instantly view all the barbershops that are close to
+            your home. You can check out their prices, services offered, and
+            more, When looking for a barber, you should consider a few things.
+            Take into consideration your price range, the location, reviews, and
+            the services offered.
+          </p>
+        </div>
       </section>
+
+      <div className="pt-5">
+        <p>
+          <Link to={"/"}>
+            <FontAwesomeIcon icon={faHome} />
+          </Link>{" "}
+          / Barbershop{" "}
+        </p>
+      </div>
     </div>
   );
 };
