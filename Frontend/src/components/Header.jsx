11a311c67ef5372797f18c32f97ef18efeaebcc0
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-22 23:22:19 +0100

    Refactor Header component to use react-bootstrap

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {Navbar,Container,Nav} from 'react-bootstrap'
+import { Navbar, Container, Nav } from "react-bootstrap";
 
-const Header = () => {
+const Header = ({ contentVisible }) => {
   return (
     <>
+     {contentVisible && (
       <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand><Link to="/" className="navbar-brand">BookMyBarber</Link></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ms-auto">
-        <Nav.Link>
-          <Link to="login" className="nav-link">Login</Link>
-        </Nav.Link>
-        <Nav.Link>
-          <Link to="register" className="nav-link">Register</Link>
-        </Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-
+        <Container>
+          <Navbar.Brand>
+            <Link to="/" className="navbar-brand">
+              BookMyBarber
+            </Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link>
+                <Link to="login" className="nav-link">
+                  Login
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="register" className="nav-link">
+                  Register
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+     )}
     </>
   );
 };
