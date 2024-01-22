4edb46efd7b8e468f22c50336c05d54587ac9ad6
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-22 23:21:56 +0100

    Add state for content visibility in App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,20 +1,22 @@
 // App.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes/Routes';
 import Header from './components/Header'; // Import your Header component
 import Footer from './components/Footer'; // Import your Footer component
 
 const App = () => {
+  const [contentVisible, setContentVisible] = useState(false);
+
   return (
     <Router>
       <div>
-        <Header /> {/* Your Header Component */}
+        <Header contentVisible={contentVisible}/> {/* Your Header Component */}
         
-        <Routes />
+        <Routes  setContentVisible={setContentVisible}/>
 
-        {/* <Footer /> Your Footer Component */}
+        {/* <Footer visible={contentVisible} /> Your Footer Component */}
       </div>
     </Router>
   );
