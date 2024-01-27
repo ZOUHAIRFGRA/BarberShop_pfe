ef26588cff646af6c397940d1e1bc190d6d60a54
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-01-27 01:30:46 +0100

    Add authActions, authReducer, and store files

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,11 +5,16 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
   </React.StrictMode>
 );
 
