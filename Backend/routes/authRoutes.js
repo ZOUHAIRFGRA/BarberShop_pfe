1b4969a4d3684631fd3c24aa14cc41c77718b173
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-10 22:38:40 +0000

    add authRoutes

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator");
+
+const {
+  registerUser,
+  registerBarber,
+  loginUser,
+  logoutUser,
+} = require("../controllers/authController");
+
+router.post("/register/user", [check("email").isEmail(), check("password").isLength({ min: 6 })], registerUser);
+router.post("/register/barber", [check("email").isEmail(), check("password").isLength({ min: 6 })], registerBarber);
+router.post("/login", [check("email").isEmail(), check("password").isLength({ min: 6 })], loginUser);
+router.post("/logout", logoutUser);
+
+module.exports = router;
