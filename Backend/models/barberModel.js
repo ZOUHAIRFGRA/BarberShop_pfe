45895fcd6708156e2902b901ae74c03a7261fcd8
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-10 22:39:46 +0000

    add barberModel

---
+++ Backend/models/barberModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const barberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  CIN: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  promoted: {
+    type: Boolean,
+    default: false,
+  },
+  image: String,
+  services: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Service',
+  }],
+  reviews: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Review',
+  }],
+  address: String,
+  latitude: String,
+  longitude: String, 
+  phone: String,
+  workingHours: [{
+    dayOfWeek: String,
+    workingHours: String,
+  }],
+  // Additional fields or validations as needed
+});
+
+const Barber = mongoose.model('Barber', barberSchema);
+
+module.exports = Barber;
