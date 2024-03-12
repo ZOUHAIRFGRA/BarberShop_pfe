a66ed038899830fce7f7ed93f33ff5e1d829477e
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-12 23:50:49 +0000

    Add barberRoutes and schedule resetAvailableSlots function

--- Backend/server.js
+++ Backend/server.js
@@ -5,8 +5,9 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const barberRoutes = require("./routes/barberRoutes");
 const connectDB = require('./config/db')
-
+const resetAvailableSlots = require('./utils/resetAvailableSlots');
 // Initialize express app
 const app = express();
 
@@ -21,8 +22,16 @@ connectDB();  // Call the MongoDB connection function
 app.use('/auth', authRoutes);
 app.use('/user',userRoutes)
 app.use('/admin',adminRoutes)
+app.use('/barber',barberRoutes)
 // Server port
 const PORT = process.env.PORT || 3000;
+const cron = require('node-cron');
+
+// Schedule the resetAvailableSlots function to run every day at midnight
+cron.schedule('0 0 * * *', async () => {
+  await resetAvailableSlots();
+  console.log('Available slots reset for all barbers');
+});
 
 // Start server
 app.listen(PORT, () => {
