c700711eace55e4624e14604a535fbe4e1357ca9
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-13 12:26:01 +0000

    Add barber routes for available slots and services

--- Backend/routes/barberRoutes.js
+++ Backend/routes/barberRoutes.js
@@ -8,6 +8,13 @@ const {
         getBarberProfile,
         updateBarberProfile,
         addServiceToBarber,
+        createAvailableSlots,
+  updateAvailableSlots,
+  deleteAvailableSlots,
+  getAllAvailableSlots,
+  updateServiceForBarber,
+  deleteServiceForBarber,
+  getAllServicesForBarber,
       
 } = require('../controllers/barberController');
 
@@ -16,6 +23,15 @@ const {
 router.get('/getprofile', authenticateUser, isBarber, getBarberProfile);
 router.put('/Updateprofile', authenticateUser, isBarber, updateBarberProfile);
 router.post('/add-service', authenticateUser, isBarber, addServiceToBarber);
+router.post('/createSlots', authenticateUser, isBarber, createAvailableSlots);
+router.put('/updateSlots', authenticateUser, isBarber, updateAvailableSlots);
+router.delete('/deleteSlots', authenticateUser, isBarber, deleteAvailableSlots);
+router.get('/getSlots', authenticateUser, isBarber, getAllAvailableSlots);
+
+
+router.get('/getServices', authenticateUser, isBarber, getAllServicesForBarber);
+router.put('/updateService', authenticateUser, isBarber, updateServiceForBarber);
+router.delete('/deleteService', authenticateUser, isBarber, deleteServiceForBarber);
 
 
 
