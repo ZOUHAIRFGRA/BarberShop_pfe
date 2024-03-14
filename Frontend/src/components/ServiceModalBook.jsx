b10b6d867780d71f1234b2056ca98090aa711902
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-14 17:13:24 +0000

    Update ServiceModalBook component to handle day selection and disable past days

--- Frontend/src/components/ServiceModalBook.jsx
+++ Frontend/src/components/ServiceModalBook.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-import { Modal, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Modal, Button, Carousel } from "react-bootstrap";
 import axios from "axios";
 import "./service.css";
 
-const ServiceModalBook = ({ show, handleClose, slots, barberId, serviceId }) => {
+const ServiceModalBook = ({
+  show,
+  handleClose,
+  slots,
+  barberId,
+  workingHours,
+  serviceId,
+}) => {
   const [selectedSlot, setSelectedSlot] = useState(null);
+  const [selectedDay, setSelectedDay] = useState(null);
   const [bookingStatus, setBookingStatus] = useState({
     success: false,
     date: null,
@@ -14,8 +22,13 @@ const ServiceModalBook = ({ show, handleClose, slots, barberId, serviceId }) => 
     setSelectedSlot(slotId);
   };
 
+  const handleDaySelection = (dayOfWeek) => {
+    setSelectedDay(dayOfWeek);
+    setSelectedSlot(null); // Reset selected slot when changing the day
+  };
+
   const handleBookAppointment = async () => {
-    if (selectedSlot) {
+    if (selectedSlot && selectedDay) {
       try {
         // Get the token from localStorage
         const token = localStorage.getItem("token");
@@ -26,11 +39,16 @@ const ServiceModalBook = ({ show, handleClose, slots, barberId, serviceId }) => 
         };
 
         // Make API call to bookAppointment route with authorization header
-        const response = await axios.post("http://localhost:4000/user/bookAppoitement", {
-          barberId,
-          serviceId,
-          slotId: selectedSlot,
-        }, { headers });
+        const response = await axios.post(
+          "http://localhost:4000/user/bookAppoitement",
+          {
+            barberId,
+            serviceId,
+            slotId: selectedSlot,
+            selectedDay,
+          },
+          { headers }
+        );
 
         console.log(response.data); // Log response from backend
         setBookingStatus({
@@ -47,6 +65,31 @@ const ServiceModalBook = ({ show, handleClose, slots, barberId, serviceId }) => 
     }
   };
 
+  const isPastDay = (dayOfWeek) => {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDayIndex = new Date().getDay(); // 0 for Sunday, 1 for Monday, etc.
+    const selectedDayIndex = daysOfWeek.indexOf(dayOfWeek);
+    return selectedDayIndex < currentDayIndex;
+  };
+
+  const isPastTime = (time) => {
+    const [hour, minute] = time.split(":").map(Number);
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const currentMinute = currentDate.getMinutes();
+    return hour < currentHour || (hour === currentHour && minute < currentMinute);
+  };
+
+  
+
   return (
     <Modal show={show} onHide={handleClose} dialogClassName="modal-xl">
       <Modal.Header closeButton>
@@ -56,35 +99,57 @@ const ServiceModalBook = ({ show, handleClose, slots, barberId, serviceId }) => 
         {bookingStatus.success ? (
           <div>
             <p>Appointment confirmed for {bookingStatus.date}</p>
-            <Button variant="primary" onClick={() => window.location.href="/appointment"}>
+            <Button
+              variant="primary"
+              onClick={() => (window.location.href = "/appointment")}
+            >
               Show Appointment
             </Button>
           </div>
         ) : (
-          <div className="slot-carousel">
-            {slots.map((slot) => (
-              <OverlayTrigger
-                key={slot._id}
-                overlay={
-                  <Tooltip id={`tooltip-${slot._id}`}>
-                    {slot.status === "booked" && "Booked"}
-                  </Tooltip>
-                }
-              >
+          <>
+            <div className="small-carousel">
+              {workingHours.map((day) => (
                 <div
-                  className={`slot-item ${slot.status === "booked" ? "disabled" : ""}`}
+                  key={day._id}
                   onClick={() => {
-                    if (slot.status !== "booked") handleSlotSelection(slot._id);
+                    if (!isPastDay(day.dayOfWeek) || day.dayOfWeek !== new Date().toLocaleDateString('en-US', { weekday: 'long' })) {
+                      handleDaySelection(day.dayOfWeek);
+                    }
+                  }}
+                  className={isPastDay(day.dayOfWeek) ? "disabled" : "active"}
+                  id="carousel-item"
+                  style={{
+                    backgroundColor:
+                      selectedDay === day.dayOfWeek ? "lightblue" : "white",
+                  }}
+                >
+                  {day.dayOfWeek}
+                </div>
+              ))}
+            </div>
+            <div className="slot-carousel">
+              {slots.map((slot) => (
+                <div
+                  key={slot._id}
+                  className={`slot-item ${
+                    slot.status === "booked" ? "disabled" : ""
+                  }`}
+                  onClick={() => {
+                    if ((!isPastTime(slot.date) && selectedDay === new Date().toLocaleDateString('en-US', { weekday: 'long' })) || slot.status !== "booked") {
+                      handleSlotSelection(slot._id);
+                    }
                   }}
                   style={{
                     backgroundColor: selectedSlot === slot._id ? "lightblue" : "white",
+                    textDecoration: slot.status === "booked" ? "line-through" : "none",
                   }}
                 >
                   {slot.date}
                 </div>
-              </OverlayTrigger>
-            ))}
-          </div>
+              ))}
+            </div>
+          </>
         )}
       </Modal.Body>
       <Modal.Footer>
