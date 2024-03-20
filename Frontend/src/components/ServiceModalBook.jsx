928ba9d1106e5548e84c697753bd9284176b2a7b
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-20 00:04:41 +0000

    fix typo in book fun

--- Frontend/src/components/ServiceModalBook.jsx
+++ Frontend/src/components/ServiceModalBook.jsx
@@ -99,7 +99,7 @@ const ServiceModalBook = ({
         console.log(selectedSlotObj.time)
         // Make API call to bookAppointment route with authorization header
         const response = await axios.post(
-          `${process.env.REACT_APP_API_URL}/user/bookAppointement`,
+          `${process.env.REACT_APP_API_URL}/user/bookAppoitement`,
           {
             barberId,
             serviceId,
