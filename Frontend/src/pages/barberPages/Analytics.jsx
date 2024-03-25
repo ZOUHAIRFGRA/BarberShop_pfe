47b174811e21d66f65b05f55d153974a4db9c33d
Zouhair Fouiguira <zouhairfgra@gmail.com>
2024-03-25 02:24:42 +0000

    Refactor Analytics component to use useEffect and useDispatch

--- Frontend/src/pages/barberPages/Analytics.jsx
+++ Frontend/src/pages/barberPages/Analytics.jsx
@@ -1,27 +1,34 @@
-import { Chart as ChartJS } from 'chart.js/auto'
-import { useSelector } from 'react-redux'
-import { Bar, Doughnut, Line } from 'react-chartjs-2'
+import { useEffect } from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { useSelector, useDispatch } from "react-redux";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import {
+  fetchAllAppointmentsForBarber,
+  fetchAllServicesForBarber,
+} from "../../actions/barberActions";
 function Statistics() {
-  const appointmentsLenght = useSelector(
-    (state) => state.barber.appointments,
-  )
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllAppointmentsForBarber());
+    dispatch(fetchAllServicesForBarber());
+  }, [dispatch]);
+  const appointments = useSelector((state) => state.barber.appointments);
 
-  const serviceCounts = []
+  const serviceCounts = [];
 
-  appointmentsLenght.forEach((appointment) => {
+  appointments.forEach((appointment) => {
     const existingService = serviceCounts.find(
-      (service) => service.name === appointment.service,
-    )
+      (service) => service.name === appointment.service.name
+    );
 
     if (existingService) {
-      existingService.count++
+      existingService.count++;
     } else {
-      serviceCounts.push({ name: appointment.service, count: 1 })
+      serviceCounts.push({ name: appointment.service, count: 1 });
     }
-  })
+  });
 
-
-  const services = useSelector((state) => state.barber.services)
+  const services = useSelector((state) => state.barber.services);
 
   return (
     <>
@@ -32,14 +39,14 @@ function Statistics() {
               labels: services.map((serv) => serv.name),
               datasets: [
                 {
-                  label: 'nombre de chaque service',
+                  label: "nombre de chaque service",
                   data: serviceCounts.map((srv) => srv.count),
                   backgroundColor: [
-                    'rgba(43, 63, 229, 0.8)',
-                    'rgba(250, 192, 19, 0.8)',
-                    'rgba(54, 162, 235, 0.8)',
-                    'rgba(153, 102, 255, 0.8)',
-                    'rgba(201, 203, 207, 0.8)',
+                    "rgba(43, 63, 229, 0.8)",
+                    "rgba(250, 192, 19, 0.8)",
+                    "rgba(54, 162, 235, 0.8)",
+                    "rgba(153, 102, 255, 0.8)",
+                    "rgba(201, 203, 207, 0.8)",
                   ],
                   borderRadius: 2,
                 },
@@ -54,14 +61,14 @@ function Statistics() {
               labels: services.map((serv) => serv.name),
               datasets: [
                 {
-                  label: 'nombre de chaque service',
+                  label: "nombre de chaque service",
                   data: serviceCounts.map((srv) => srv.count),
                   backgroundColor: [
-                    'rgba(43, 63, 229, 0.8)',
-                    'rgba(250, 192, 19, 0.8)',
-                    'rgba(54, 162, 235, 0.8)',
-                    'rgba(153, 102, 255, 0.8)',
-                    'rgba(201, 203, 207, 0.8)',
+                    "rgba(43, 63, 229, 0.8)",
+                    "rgba(250, 192, 19, 0.8)",
+                    "rgba(54, 162, 235, 0.8)",
+                    "rgba(153, 102, 255, 0.8)",
+                    "rgba(201, 203, 207, 0.8)",
                   ],
                   borderRadius: 2,
                 },
@@ -76,14 +83,14 @@ function Statistics() {
               labels: services.map((serv) => serv.name),
               datasets: [
                 {
-                  label: 'nombre de chaque service',
+                  label: "nombre de chaque service",
                   data: serviceCounts.map((srv) => srv.count),
                   backgroundColor: [
-                    'rgba(43, 63, 229, 0.8)',
-                    'rgba(250, 192, 19, 0.8)',
-                    'rgba(54, 162, 235, 0.8)',
-                    'rgba(201, 203, 207, 0.8)',
-                    'rgba(153, 102, 255, 0.8)',
+                    "rgba(43, 63, 229, 0.8)",
+                    "rgba(250, 192, 19, 0.8)",
+                    "rgba(54, 162, 235, 0.8)",
+                    "rgba(201, 203, 207, 0.8)",
+                    "rgba(153, 102, 255, 0.8)",
                   ],
                 },
               ],
@@ -92,7 +99,7 @@ function Statistics() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Statistics
+export default Statistics;
